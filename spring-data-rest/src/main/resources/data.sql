c002d3d37b3ab911f6106f9bb44567a7da849ce5
Juan Lamadrid <juan.lamadrid@gmail.com>
2015-01-26 18:40:52 -0500

    added sample data to spring-rest-services

---
+++ spring-data-rest/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO tags (name, description, category) VALUES  ('options', 'option based trades','etf');
+INSERT INTO tags (name, description, category) VALUES  ('snp', 'snp 500','indexes');
+INSERT INTO tags (name, description, category) VALUES  ('metals', 'metals trades','futures');
